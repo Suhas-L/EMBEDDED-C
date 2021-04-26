bc5b5843bb7e887346401c8d1ffc442f0fde7e64
Suhas L <80455876+Suhas-L@users.noreply.github.com>
2021-04-26 08:47:25 +0100

    Update lib.h

--- inc/lib.h
+++ inc/lib.h
@@ -1,1 +1,18 @@
+#ifndef __LIB_H__
+#define __LIB_H__
+/**
+ * @file lib.h
+ * @author Suhas-L
+ * @brief User defined utilities used across the project
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+#endif /* __LIB_H__ */
 
