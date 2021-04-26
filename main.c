ffdebba12e7d88571a4e82faa659c9d8fd305078
Suhas L <80455876+Suhas-L@users.noreply.github.com>
2021-04-26 08:49:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
 
+
+#include "lib.h"
+
+int main(void)
+{
+    DDRB|=(1<<PB0);
+    DDRD&=~(2<<PD0);
+    DDRD&=~(4<<PD0);
+    PORTD|=(2<<PD0);
+    PORTD|=(4<<PD0);
+
+    while(1)
+{
+    if(!(PIND&(2<<PD0)) && !(PIND&(4<<PD0)))
+    {PORTB|=(1<<PB0);
+    _delay_ms(200);}
+    else{
+    PORTB&=~(1<<PB0);
+    _delay_ms(200);}
+
+}
+
+    return 0;
+}
+
