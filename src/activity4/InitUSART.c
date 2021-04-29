dc8f19a15f5bd846fc44d170d16a84bc74641874
Suhas L <80455876+Suhas-L@users.noreply.github.com>
2021-04-29 18:08:43 +0100

    Update InitUSART.c

--- src/activity4/InitUSART.c
+++ src/activity4/InitUSART.c
@@ -1,1 +1,8 @@
+#include "lib.h"
+void InitUSART(uint16_t ubrr_value){
+UBRR0L = ubrr_value;
+UBRR0H=(ubrr_value>>8)&0x00ff;
+UCSR0C=(1<<UCSZ00)|(1<<UCSZ01);
+UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
+}
 
