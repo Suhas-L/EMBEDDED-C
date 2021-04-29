4db066f720641bb1d1969fa16b13e09f1c846190
Suhas L <80455876+Suhas-L@users.noreply.github.com>
2021-04-29 17:26:42 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,54 @@
-
-
 #include "lib.h"
-
 int main(void)
 {
-    DDRB|=(1<<PB0);
-    DDRD&=~(2<<PD0);
-    DDRD&=~(4<<PD0);
-    PORTD|=(2<<PD0);
-    PORTD|=(4<<PD0);
-
-    while(1)
+   Button_setup();
+   LED_setup;
+   InitADC();
+   uint16_t temp,i;
+   pwmSet();
+while(1)
 {
-    if(!(PIND&(2<<PD0)) && !(PIND&(4<<PD0)))
-    {PORTB|=(1<<PB0);
-    _delay_ms(200);}
-    else{
-    PORTB&=~(1<<PB0);
-    _delay_ms(200);}
+    if(Heater_ON && PersonSeated)
+    {
+        LED_ON;
+       InitUSART(103);
+        temp=ReadADC(0);
+        if(temp<=200)
+        {
+            PWM_TempValue=51;
+            char temp_value[33] = "Temperature value=20 deg Celsius";
+            for(i=0;i<33;i++){USARTWrite_char(temp_value[i]);}
+            _delay_ms(200);
+
+        }
+        else if(temp<=500 && temp>=210)
+        {
+            PWM_TempValue=102;
+            char temp_value[33] = "Temperature value=25 deg Celsius";
+            for(i=0;i<33;i++){USARTWrite_char(temp_value[i]);}
+            _delay_ms(200);
+        }
+        else if(temp<=700 && temp>=510)
+        {
+            PWM_TempValue=179;
+           char temp_value[33] ="Temperature value=29 deg Celsius";
+            for(i=0;i<33;i++){USARTWrite_char(temp_value[i]);}
+            _delay_ms(200);
+        }
+        else if(temp<=1024 && temp>=710)
+        {
+            PWM_TempValue=242;
+            char temp_value[33] = "Temperature value=33 deg Celsius";
+           for(i=0;i<33;i++){USARTWrite_char(temp_value[i]);}
+            _delay_ms(200);
+        }
+
+    }
+    else
+    {
+        LED_OFF;
+        _delay_ms(200);
+    }
 
 }
 
